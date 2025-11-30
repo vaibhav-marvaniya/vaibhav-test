46a600d073fa7b267ca00f548be0534368d8f3e1
vaibhav-marvaniya <vaibhav.mv76@gmail.com>
2025-11-30 13:07:39 +0530

    Card Prefab added and Dynamic Grid Logic done

--- Assets/_Dev/Scripts/GameManager.cs
+++ Assets/_Dev/Scripts/GameManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int RowCount, ColCount;
+
+    public static GameManager instance
     {
-        
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+        private set
+        {
+            _instance = value;
+        }
+    }
+    private static GameManager _instance;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/_Dev/Scripts/GridManager.cs
+++ Assets/_Dev/Scripts/GridManager.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public RectTransform GameplayCanvas;
+    public int RowCount, ColCount;
+    public GameObject CardPrefab;
+    public Transform CardParent;
+    public GridLayoutGroup CardGrid;
+    public Vector2 CardOrigVector;
+    public Vector2 UnusedAreaVector;
+    [SerializeField]
+    public Vector2 RequiredCardVector, CardOrigSpace;
+
     void Start()
     {
-        
+        CardOrigVector = CardPrefab.GetComponent<RectTransform>().sizeDelta;
+      
+        Debug.Log(GameplayCanvas.rect.size);
+        Debug.Log(CardParent.transform.GetComponent<RectTransform>().rect.size);
+
+        //CardGrid.cellSize = new Vector2(240, 320);
+        //CardGrid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        //CardGrid.constraintCount = 5;
+        //CardGrid.spacing = new Vector2(100, 100);
+        FindCardUnusedArea();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FindCardUnusedArea()
     {
-        
+        RowCount = GameManager.instance.RowCount;
+        ColCount = GameManager.instance.ColCount;
+        Vector2 AreaforCards = new Vector2(CardParent.transform.GetComponent<RectTransform>().rect.width - CardGrid.padding.right - CardGrid.padding.left - (CardGrid.spacing.x * (ColCount - 1)),
+            CardParent.transform.GetComponent<RectTransform>().rect.height - CardGrid.padding.top - CardGrid.padding.bottom - (CardGrid.spacing.y * (RowCount - 1)));
+        CardOrigSpace.x = CardOrigVector.x * ColCount;
+        CardOrigSpace.y = CardOrigVector.y * RowCount;
+        Debug.Log(AreaforCards + " " + CardOrigSpace);
+
+        if (CardOrigSpace.x - AreaforCards.x > CardOrigSpace.y - AreaforCards.y)
+        {
+            RequiredCardVector.x = AreaforCards.x / ColCount;
+            RequiredCardVector.x = Mathf.FloorToInt(RequiredCardVector.x);
+            RequiredCardVector.y = (RequiredCardVector.x * CardOrigVector.y) / CardOrigVector.x;
+            Debug.Log(RequiredCardVector.x + " "+ RequiredCardVector.y);
+        }
+        else if (CardOrigSpace.y - AreaforCards.y > CardOrigSpace.x - AreaforCards.x)
+        {
+            RequiredCardVector.y = AreaforCards.y / RowCount;
+            RequiredCardVector.y = Mathf.FloorToInt(RequiredCardVector.y);
+            RequiredCardVector.x = (RequiredCardVector.y * CardOrigVector.x) / CardOrigVector.y;
+            Debug.Log(RequiredCardVector.x + " " + RequiredCardVector.y);
+        }
+        CardGrid.cellSize = RequiredCardVector;
+
+        CardGrid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        CardGrid.constraintCount = ColCount;
+
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        GameObject go;
+        for (int i = 0; i < RowCount; i++)
+        {
+            for (int j = 0; j < ColCount; j++)
+            {
+                go = Instantiate(CardPrefab, CardParent);
+                go.GetComponent<CardScript>().SetCardData(i, j);
+            }
+        }
     }
 }
